e58ec07e2b097e30a90b9bd45f25f737a786546c
derkalaender <git@derkalaender.de>
2020-06-20 10:25:35 +0200

    Use annotation subproject in tests

--- compiler-plugin/src/test/kotlin/de/derkalaender/jvmbuilder/compiler/JvmBuilderTest.kt
+++ compiler-plugin/src/test/kotlin/de/derkalaender/jvmbuilder/compiler/JvmBuilderTest.kt
@@ -9,8 +9,62 @@ import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldContain
 import org.jetbrains.kotlin.config.JvmTarget
 
+const val defaultAnnotation = "de.derkalaender.jvmbuilder.annotations.JvmBuilder"
+
 class JvmBuilderTest : StringSpec() {
-  private val jvmBuilderAnnotation =
+  init {
+    "any annotation works" {
+      val result =
+          compile(
+              createCustomAnnotation("SomeAnnotation"),
+              kotlin(
+                  "NonDataClass.kt",
+                  """
+                  package de.derkalaender.jvmbuilder.test
+          
+                  @SomeAnnotation
+                  data class SomeAnnotatedClass(val someProp: String)
+                  """.trimIndent()),
+              annotation = "de.derkalaender.jvmbuilder.test.SomeAnnotation")
+      result.exitCode shouldBe KotlinCompilation.ExitCode.OK
+    }
+
+    "data is required" {
+      val result =
+          compile(
+              kotlin(
+                  "NonDataClass.kt",
+                  """
+                  package de.derkalaender.jvmbuilder.test
+                  
+                  import de.derkalaender.jvmbuilder.annotations.JvmBuilder
+          
+                  @JvmBuilder
+                  class NonDataClass
+                  """.trimIndent()))
+      result.exitCode shouldBe KotlinCompilation.ExitCode.COMPILATION_ERROR
+      result.messages shouldContain
+          "NonDataClass.kt: (5, 1): @JvmBuilder is only supported on data classes!"
+    }
+
+    "simple test" {
+      val result =
+          compile(
+              kotlin(
+                  "SimpleTest.kt",
+                  """
+                  package de.derkalaender.jvmbuilder.test
+                  
+                  import de.derkalaender.jvmbuilder.annotations.JvmBuilder
+          
+                  @JvmBuilder
+                  data class SimpleTest(val defaultHaving: Boolean = true, val notDefaultHaving: Boolean)
+                  """.trimIndent()))
+      result.exitCode shouldBe KotlinCompilation.ExitCode.OK
+    }
+  }
+
+  private fun createCustomAnnotation(name: String = "JvmBuilder") =
       kotlin(
           "JvmBuilder.kt",
           """
@@ -18,37 +72,21 @@ class JvmBuilderTest : StringSpec() {
           
           @Retention(AnnotationRetention.BINARY)
           @Target(AnnotationTarget.CLASS)
-          annotation class JvmBuilder
+          annotation class $name
           """.trimIndent())
 
-  init {
-    "dataIsRequired" {
-      val result =
-          compile(
-              kotlin(
-                  "NonDataClass.kt",
-                  """
-                  package de.derkalaender.jvmbuilder.test
-          
-                  @JvmBuilder
-                  class NonDataClass
-                  """.trimIndent()))
-      result.exitCode shouldBe KotlinCompilation.ExitCode.COMPILATION_ERROR
-      result.messages shouldContain "NonDataClass.kt: (3, 1): @JvmBuilder is only supported on data classes!"
-    }
-  }
-
-  private fun prepareCompilation(vararg sourceFiles: SourceFile): KotlinCompilation {
+  private fun prepareCompilation(
+      vararg sourceFiles: SourceFile, annotation: String = defaultAnnotation
+  ): KotlinCompilation {
     return KotlinCompilation().apply {
       commandLineProcessors = listOf(JvmBuilderCommandLineProcessor())
       compilerPlugins = listOf(JvmBuilderPlugin())
       pluginOptions =
           listOf(
               createPluginOption("enabled", true),
-              createPluginOption(
-                  "jvmBuilderAnnotation", "de.derkalaender.jvmbuilder.test.JvmBuilder"))
+              createPluginOption("jvmBuilderAnnotation", annotation))
       inheritClassPath = true
-      sources = sourceFiles.asList() + jvmBuilderAnnotation
+      sources = sourceFiles.asList()
       verbose = false
       jvmTarget = JvmTarget.fromString(System.getenv()["ci_java_version"] ?: "1.8")!!.description
     }
@@ -58,7 +96,9 @@ class JvmBuilderTest : StringSpec() {
     return PluginOption("jvm-builder-compiler-plugin", name, value.toString())
   }
 
-  private fun compile(vararg sourceFiles: SourceFile): KotlinCompilation.Result {
-    return prepareCompilation(*sourceFiles).compile()
+  private fun compile(
+      vararg sourceFiles: SourceFile, annotation: String = defaultAnnotation
+  ): KotlinCompilation.Result {
+    return prepareCompilation(*sourceFiles, annotation = annotation).compile()
   }
 }
